3c75ee9a3c0a0a1bb9efe0e7ef38b8803847ad70
sgj011206 <sgj20011206@gmail.com>
2026-04-30 17:42:39 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-fn main() {
-    let result = add(5, 10);
-    println!("The result is: {}", result);
-}
+
 fn add(a: i32, b: i32) -> i32 {
     a + b 
 }
 
+fn main() {
+    let result = add(5, 10);
+    println!("The result is: {}", result);
+}
 #[cfg(test)]
 mod tests {
     use super::*;
